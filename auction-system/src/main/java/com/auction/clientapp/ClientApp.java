7c2cf1c7884698e0d102821c8b70305ed5794232
haianh <buianhmonke@gmail.com>
2026-05-11 09:53:41 +0700

    .

---
+++ auction-system/src/main/java/com/auction/clientapp/ClientApp.java
@@ -0,0 +1,28 @@
+package com.auction.clientapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ClientApp extends Application {
+    public static void main(String[] args){launch(args);}
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginFrame.fxml"));
+        Parent root = loader.load();
+
+        Scene scene= new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Auction System");
+        stage.show();
+
+
+
+
+
+    }
+
+}
---
+++ auction-system/src/main/java/com/auction/clientapp/ItemFrameController.java
@@ -0,0 +1,25 @@
+package com.auction.clientapp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ItemFrameController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+    public void back(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
